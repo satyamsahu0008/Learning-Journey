4b34d49c4ebb1a42c1e9bbead46c2a93971da08c
satyamsahu <Satyamkumarsahu47@gmail.com>
2026-08-24 23:56:39 +0530

    Add Python journey basics

---
+++ Python-Journey/Basic 1.1.py
@@ -0,0 +1,15 @@
+### Write a program to print your:Name,Age, Course
+#Type 1
+name = "Satyam Sahu"
+age = "18"
+course = "B.Tech CSE (AI/ML)"
+print(name, age, course)
+
+print(name)
+print(age)
+print(course)
+
+#Type 2
+print("Name: Satyam")
+print("Age: 18")
+print("Dream: AI Engineer")
